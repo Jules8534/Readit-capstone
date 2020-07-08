8e8848ea7805163033f1b59f0906efc260789865
DavidRinSE <davidrichardsonin@gmail.com>
2020-07-08 10:28:50 -0400

    added login page and stuffs

--- readit/urls.py
+++ readit/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from readit_site.urls import urlpatterns as readiturls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += readiturls
--- readit_site/views.py
+++ readit_site/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from .forms import LoginForm
 # Create your views here.
+@login_required
+def index(request):
+    html = "index.html"
+    username = request.user.username
+    return render(request, html, {"name": username})
+
+def login_view(request):
+    html = "loginform.html"
+    
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(
+                    request.GET.get('next', reverse('homepage'))
+                )
+    form = LoginForm()
+    return render(request, html, {"form":form})
