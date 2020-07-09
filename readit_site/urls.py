d32576e5bcc093756e6fad0f0dccc1dc51253381
Sean Bailey <sepabailey@gmail.com>
2020-07-09 10:26:43 -0400

    added password change form and logout button

--- readit_site/urls.py
+++ readit_site/urls.py
@@ -3,5 +3,8 @@ from readit_site import views
 
 urlpatterns = [
     path('', views.index, name="homepage"),
-    path('login/', views.login_view),
+    path('login/', views.login_view, name='login_url'),
+    path('logout/', views.logoutview, name='logout_url'),
+    path('change_password.html',
+         views.change_password, name='change_password'),
 ]
--- readit_site/views.py
+++ readit_site/views.py
@@ -1,26 +1,58 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
 from .forms import LoginForm
 # Create your views here.
+
+
 @login_required
 def index(request):
     html = "index.html"
     username = request.user.username
     return render(request, html, {"name": username})
 
+
 def login_view(request):
     html = "loginform.html"
-    
+
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            user = authenticate(request, username=data['username'], password=data['password'])
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
             if user:
                 login(request, user)
-                return HttpResponseRedirect(
-                    request.GET.get('next', reverse('homepage'))
-                )
+            return HttpResponseRedirect(
+                request.GET.get('next', reverse('homepage'))
+            )
     form = LoginForm()
-    return render(request, html, {"form":form})
+    return render(request, html, {"form": form})
+
+
+# https://simpleisbetterthancomplex.com/tips/2016/08/04/django-tip-9-password-change-form.html
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(
+                request, 'Your password was successfully updated!')
+            return HttpResponseRedirect(reverse('homepage'))
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {
+        'form': form
+    })
+
+
+@login_required
+def logoutview(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('homepage'))
