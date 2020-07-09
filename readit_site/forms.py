2fd85ff3dd5eb9fe544759f03bcdb72fea7a25ff
Julita Marshall <julitamcg5@gmail.com>
2020-07-09 08:27:30 -0500

    created user_signup

--- readit_site/forms.py
+++ readit_site/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from .models import ReaditUserModel
+from django.contrib.auth.forms import UserCreationForm
+
+from readit_site.models import ReaditUserModel
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput)
+
+class ReaditUserModelForm(UserCreationForm):
+    email = forms.EmailField(max_length=60, help_text='Required. Add a valid email address')
+
+    class Meta:
+         model = ReaditUserModel
+         fields = ("email", "username", "password1", "password2")
--- readit_site/views.py
+++ readit_site/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.shortcuts import render, reverse, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
-from .forms import LoginForm
+from .forms import LoginForm, ReaditUserModelForm
 # Create your views here.
 @login_required
 def index(request):
@@ -9,18 +9,41 @@ def index(request):
     username = request.user.username
     return render(request, html, {"name": username})
 
+
 def login_view(request):
     html = "loginform.html"
-    
+
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            user = authenticate(request, username=data['username'], password=data['password'])
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
             if user:
                 login(request, user)
                 return HttpResponseRedirect(
                     request.GET.get('next', reverse('homepage'))
                 )
     form = LoginForm()
-    return render(request, html, {"form":form})
+    return render(request, html, {"form": form})
+
+
+def readitusermodel_view(request):
+    context = {}
+    if request.POST:
+        form = ReaditUserModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            print(password)
+            account = authenticate(username=username, password=password)
+            print(account)
+            login(request, account)
+            return redirect('homepage')
+        else:  # GET request
+            context['readitusermodel_form'] = form
+    else:
+        form = ReaditUserModelForm()
+        context['readitusermodel_form'] = form
+    return render(request, 'register.html', context)
